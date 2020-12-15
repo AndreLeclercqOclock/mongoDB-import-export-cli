f3543520fef830a09bdc02dbac219af687add7d1
André <a.leclercq@coprometal.com>
2020-12-15 11:51:59 +0100

    First Commit

---
+++ index.js
@@ -0,0 +1,15 @@
+
+/**
+ * TODO: Write README.MD
+ * TODO: Create GITHUB repo
+ * TODO: Config file
+ * TODO: Use Shell in JS
+ * TODO: Create CLI command
+ * TODO: mcli config edit || Edit config file
+ * TODO: mcli config show || Show the config file
+ * TODO: mcli import --c=collection_name || Import from JSON file to MongoDB
+ * TODO: mcli export --c=collection_name || Export from mongoDB to JSON file
+ * TODO: mcli help || Show all command
+ */
+
+
