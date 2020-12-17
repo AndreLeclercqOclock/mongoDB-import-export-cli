be4f50b2b49b251d4a86775651de76779eedb998
André <a.leclercq@coprometal.com>
2020-12-17 15:00:13 +0100

    update 1.3.0

--- index.js
+++ index.js
@@ -1,30 +1,11 @@
 #!/usr/bin/env node
 
-// TODO: Add backup commands
-
 const { exec } = require('child_process')
 const readline = require('readline')
-const config = require('./config_mcli.json')
 const fs = require('fs')
-let dataConfig = config
-let collection = ''
-let jsonFile = ''
+const config = require('./config_mcli.json')
 
-
-process.argv.forEach(a => {
-    if (a.substring(0, 4) === '--c=') {
-        collection = a.replace(a.substring(0, 4), '')
-    } else if (a.substring(0, 4) === '--f='){
-        jsonFile = a.replace(a.substring(0, 4), '')
-    }
-})
-
-if(jsonFile === ''){
-    jsonFile = `${config.folder}/${collection}.json`
-} else if(jsonFile.substr(jsonFile.length - 5) !== '.json') {
-    jsonFile += '.json'
-}
-
+// Manage arguments from command-line
 if(config.db === '' || config.folder === ''){
     editConfig()
 } else if(process.argv.includes('config')){
@@ -34,67 +15,127 @@ if(config.db === '' || config.folder === ''){
         editConfig()
     }
 } else if(process.argv.includes('export')){
-    if(collection !== ''){
-        exec(`mongoexport --collection=${collection} --db=${config.db} --out=${jsonFile}`, (e, stdout, stderr) => {
-            if (e) {
-                console.log(`error: ${e.message}`)
-                return
-            }
-            if (stderr) {
-                console.log(`${stderr}`)
-                return
-            }
-            console.log(`stdout: ${stdout}`)
-        });
-    } else console.log('Collection not defined, add arguments --c=collection in your command line')
-
+    impExp('mongoexport')
 } else if(process.argv.includes('import')){
-    if(collection){
-        exec(`mongoimport --collection=${collection} --db=${config.db} --drop --file=${jsonFile}`, (e, stdout, stderr) => {
-            if (e) {
-                console.log(`error: ${e.message}`)
-                return
-            }
-            if (stderr) {
-                console.log(`${stderr}`)
-                return
-            }
-            console.log(`stdout: ${stdout}`)
-        });
-    } else console.log('Collection not defined, add arguments --c=collection in your command line')
-} else if(process.argv.includes('help')){
-    console.log('')
-    console.log("Usage : mcli < command >")
-    console.log("where command is one of :")
-    console.log(" config edit (Edit config file)")
-    console.log(" config show (Show config file)")
-    console.log(" import --c=myCollection (Import collection 'myCollection' from JSON file)")
-    console.log(" export --c=myCollection (Export collection 'myCollection' to JSON file)")
-    console.log(" import --c=myCollection --f=my-folder/my-file.json (Import collection 'MyCollection' from specific JSON file")
-    console.log(" export --c=myCollection --f=my-folder/my-file.json (Export collection 'MyCollection' to specific JSON file")
-    console.log(" help (List all commands)")
-    console.log('')
+    impExp('mongoimport')
+} else if(process.argv.includes('backup')) {
+    backup()
+}else if(process.argv.includes('help')){
+    console.log(` 
+Usage : mcli < command >
+where command is one of :
+ config edit (Edit config file)
+ config show (Show config file)
+ import --c=myCollection (Import collection 'myCollection' from JSON file)
+ export --c=myCollection (Export collection 'myCollection' to JSON file)
+ import --c=myCollection --f=my-folder/my-file.json (Import collection 'MyCollection' from specific JSON file
+ export --c=myCollection --f=my-folder/my-file.json (Export collection 'MyCollection' to specific JSON file
+ help (List all commands)
+ `)
 }
 
+/**
+ * BackUp and Compress BackUp all Database
+ * @function
+ */
+function backup(){
+    let backUpName = `backUp_${Date.now()}`
+    let backUpFolder = `${config.folder}/${backUpName}`
+    exec(`mongodump --db=${config.db} --out=${backUpFolder}`, (e, stdout, stderr) => {
+        if(e){
+            console.log(`error: ${e.message}`)
+            return
+        }
+        if(stderr){
+            console.log(`${stderr}`)
+            if(process.argv.includes('--tar')){
+                exec(`tar -czvf ${backUpFolder}.tar.gz ${backUpFolder}`, (e, stdout, stderr) => {
+                    if(e){
+                        console.log(`error: ${e.message}`)
+                        return
+                    }
+                    if(stderr){
+                        console.log(`${stderr}`)
+                        exec(`cd ${config.folder} && rm -rf ${backUpName}`, (e, stdout, stderr) => {
+                            if(e){
+                                console.log(`error: ${e.message}`)
+                                return
+                            }
+                            if(stderr){
+                                console.log(`${stderr}`)
+                                return
+                            }
+                            console.log(`stdout: ${stdout}`)
+                        })
+                        return
+                    }
+                    console.log(`stdout: ${stdout}`)
 
+                })
+            }
+            return
+        }
+        console.log(`stdout: ${stdout}`)
+    })
+
+}
+
+/**
+ * Import / Export collection from/to JSON
+ * @function
+ * @param action {string}
+ */
+function impExp(action){
+    let cArg = action === 'mongoimport' ? '--drop --file=' : '--out='
+    let collection = ''
+    let jsonFile = ''
+
+    process.argv.forEach(a => {
+        if (a.substring(0, 4) === '--c=')
+            collection = a.replace(a.substring(0, 4), '')
+        else if (a.substring(0, 4) === '--f=')
+            jsonFile = a.replace(a.substring(0, 4), '')
+    })
+
+    if(jsonFile === '')
+        jsonFile = `${config.folder}/${collection}.json`
+    else if(jsonFile.substr(jsonFile.length - 5) !== '.json')
+        jsonFile += '.json'
+
+    if(collection !== ''){
+        exec(`${action} --collection=${collection} --db=${config.db} ${cArg}${jsonFile}`, (e, stdout, stderr) => {
+            if(e){
+                console.log(`error: ${e.message}`)
+                return
+            }
+            if(stderr){
+                console.log(`${stderr}`)
+                return
+            }
+            console.log(`stdout: ${stdout}`)
+        })
+    } else console.log('Collection not defined, add arguments --c=collection in your command line')
+}
+
+/**
+ * Edit config file
+ * @function
+ */
 function editConfig(){
+    let dataConfig = config
     const rl = readline.createInterface({
         input: process.stdin,
         output: process.stdout
     })
     rl.question(`What's your database Name ? `, (dbName) => {
         rl.question(`What's the default folder to save JSON files ? `, (folder) => {
-            dataConfig.db = dbName
             if(folder.slice(-1) === '/')
                 folder = folder.slice(0, -1)
             dataConfig.folder = folder
+            dataConfig.db = dbName
             console.log('The config file has been updated')
             rl.close()
-            writeConfig()
+            fs.writeFile('./node_modules/mongodb-import-export-cli/config_mcli.json', JSON.stringify(dataConfig, null, 4), e => e ? console.log(e) : null)
         })
     })
-}
-
-function writeConfig(){
-    fs.writeFile('./node_modules/mongodb-import-export-cli/config_mcli.json', JSON.stringify(dataConfig, null, 4), e => e ? console.log(e) : null)
 }
